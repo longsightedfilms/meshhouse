33031071393eae507f0ba8587fc40da4680125b6
Maxim Makarov <aks11311@gmail.com>
2019-09-21 20:00:12 +0300

    Initial release

---
+++ vue.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    },
+    electronBuilder: {
+      externals: ['node-notifier', 'electron-store'],
+      nodeModulesPath: ['../../node_modules', './node_modules'],
+      builderOptions: {
+        appId: 'com.longsightedfilms.meshhouse',
+        productName: 'MeshHouse',
+        compression: 'maximum',
+        asar: true,
+        mac: {
+          category: 'public.app-category.graphics-design',
+          target: 'dmg'
+        },
+        nsis: {
+          oneClick: false,
+          installerIcon: './build/icons/icon.ico',
+          license: './build/license.txt',
+          allowToChangeInstallationDirectory: true
+        }
+      }
+    }
+  },
+
+  productionSourceMap: false
+}
